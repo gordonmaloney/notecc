64db30c5c69bd3f1e3b13bbf70583bf13185e4f8
gordonmaloney <gordonmaloney@gmail.com>
2025-08-12 11:56:26 +0100

    not ecc

--- src/Mailer/Prompts.jsx
+++ src/Mailer/Prompts.jsx
@@ -1,33 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Mailer from "./Mailer";
+import { Button, TextField } from "@mui/material";
 
 const Prompts = () => {
-
-
-    
   const [template, setTemplate] = useState(`test1,
 
-<<|test2|>>
+<<|userStory|>>
 
-<<|test3|>>`);
+<<|userName|>>`);
 
+  const [answers, setAnswers] = useState([]);
 
+  const [userName, setUserName] = useState("");
+  const [userStory, setUserStory] = useState("");
 
-  const [answers, setAnswers] = useState(['testAnswer1', 'testAnswer2']);
-
-
-
-  useEffect(() => {
+  const handlePrompts = () => {
     setTemplate(
       template
-        .replace("<<|test2|>>", answers[0])
-        .replace("<<|test3|>>", answers[1])
+        .replace("<<|userStory|>>", userName)
+        .replace("<<|userName|>>", userStory)
     );
-  }, []);
+    setAnswers([userName, userStory]);
+  };
 
   return (
     <div>
       test
+      <TextField
+        label="Your Name"
+        variant="outlined"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        label="Your Story"
+        variant="outlined"
+        value={userStory}
+        onChange={(e) => setUserStory(e.target.value)}
+        multiline
+        rows={4}
+        fullWidth
+      />
+      <Button onClick={handlePrompts}>Next</Button>
       <Mailer template={template} setTemplate={setTemplate} answers={answers} />
     </div>
   );
